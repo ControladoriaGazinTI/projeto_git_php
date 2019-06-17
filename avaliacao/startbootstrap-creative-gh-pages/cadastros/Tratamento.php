a66db1b2ae814aa211b0b84570ccc2838de30b15
KiraGamesBR <romuloevil@outlook.com>
2019-06-17 11:44:06 -0300

    update

--- avaliacao/startbootstrap-creative-gh-pages/cadastros/Tratamento.php
+++ avaliacao/startbootstrap-creative-gh-pages/cadastros/Tratamento.php
@@ -1,24 +1,36 @@
-<form action="">
+<?php
+require_once 'class/Tratamento.php';
+$tratamento = new Tratamento;
+if (isset($_POST["enviar"])) {
+    $tratamento->setExercicio($_POST["exercicio"]);
+    $tratamento->setAlimentacao($_POST["alimentacao"]);
+    $tratamento->setMedicamento($_POST["medicamento"]);
+    $tratamento->setTipoTratamento($_POST["tipo_tratamento"]);
+    print_r($tratamento);
+}
+?>
+<form action="" method="post">
     <div class="form-group">
-    <label>Tipo do tratamento:</label>
+        <label>Tipo do tratamento:</label>
         <select name="tipo_tratamento" id="" class="form-control">
-                <option value="1">A baixo do peso</option>
-                <option value="2">Levemente acima do Peso</option>
-                <option value="3">Obesidade grau I</option>
-                <option value="4">Obesidade grau II (Severa)</option>
-                <option value="5">Obesidade grau III (Morbida)</option>
+            <option value="1">A baixo do peso</option>
+            <option value="2">Levemente acima do Peso</option>
+            <option value="3">Obesidade grau I</option>
+            <option value="4">Obesidade grau II (Severa)</option>
+            <option value="5">Obesidade grau III (Morbida)</option>
         </select>
     </div>
     <div class="form-group">
         <label for="">Exercicios</label>
-        <textarea name="exercicio" id="" cols="30" rows="10" class="form-control"></textarea>
+        <textarea name="exercicio" id="" cols="20" rows="5" class="form-control"></textarea>
     </div>
     <div class="form-group">
         <label for="">Alimentação:</label>
-        <textarea name="alimentacao" id="" cols="30" rows="10" class="form-control"></textarea>
+        <textarea name="alimentacao" id="" cols="20" rows="5" class="form-control"></textarea>
     </div>
     <div class="form-group">
         <label for="">Medicamento:</label>
-        <textarea name="medicamento" id="" cols="30" rows="10" class="form-control"></textarea>
+        <textarea name="medicamento" id="" cols="20" rows="5" class="form-control"></textarea>
     </div>
+    <button type="submit" class="btn btn-success" name="enviar">Cadastrar</button>
 </form>
