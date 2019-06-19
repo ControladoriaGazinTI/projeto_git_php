712a7e46f72606b40dc0d494e0cf5e128335aa0b
KiraGamesBR <romuloevil@outlook.com>
2019-06-19 11:53:33 -0300

    pegar id do tratamento aleatorio

--- avaliacao/startbootstrap-creative-gh-pages/class/Resultado.php
+++ avaliacao/startbootstrap-creative-gh-pages/class/Resultado.php
@@ -6,7 +6,8 @@ class Resultado extends Usuario
     private $imc;
     private $classificacao;
     private $idUsuario;
-    private $idtratamento;
+    private $tipo_tratamento;
+    private $idTratamento;
 
     public function __construct()
     { }
@@ -14,6 +15,10 @@ class Resultado extends Usuario
     public function getData()
     {
         return $this->data;
+    }
+    public function getIdTratamento()
+    {
+        return $this->idTratamento;
     }
     public function getImc()
     {
@@ -23,9 +28,9 @@ class Resultado extends Usuario
     {
         return $this->classificacao;
     }
-    public function getIdTratamento()
+    public function getTipoTratamento()
     {
-        return $this->idtratamento;
+        return $this->tipo_tratamento;
     }
     public function getIdUsuario()
     {
@@ -45,13 +50,17 @@ class Resultado extends Usuario
     {
         $this->classificacao = $classificacao;
     }
-    public function setIdTratamento($tratamento)
+    public function setTipoTratamento($tratamento)
     {
-        $this->idtratamento = $tratamento;
+        $this->tipo_tratamento = $tratamento;
     }
     public function setIdUsuario($objeto)
     {
         $this->idUsuario = $objeto;
+    }
+    public function setIdTratamento($t)
+    {
+        $this->idTratamento = $t;
     }
     public function calcularImc()
     {
@@ -62,22 +71,32 @@ class Resultado extends Usuario
     }
     public function classificacao()
     {
+
+
         if ($this->getImc() < 0) {
             $this->setClassificacao("IMC inválido");
+            $this->setTipoTratamento(0);
         } elseif ($this->getImc() < 18.5) {
             $this->setClassificacao("Abaixo do Peso.");
+            $this->setTipoTratamento(1);
         } elseif ($this->getImc() < 25) {
             $this->setClassificacao("Peso ideal!!!Parabéns.");
+            $this->setTipoTratamento(2);
         } elseif ($this->getImc() < 30) {
             $this->setClassificacao("Levemente acima do Peso.");
+            $this->setTipoTratamento(3);
         } elseif ($this->getImc() < 35) {
             $this->setClassificacao("Obesidade grau 1.");
+            $this->setTipoTratamento(4);
         } elseif ($this->getImc() < 40) {
             $this->setClassificacao("Obesidade grau 2 (severa).");
+            $this->setTipoTratamento(5);
         } elseif ($this->getImc() < 60) {
             $this->setClassificacao("Obesidade grau 3 (mórbida) ");
+            $this->setTipoTratamento(6);
         } else {
             $this->setClassificacao("IMC inválido");
+            $this->setTipoTratamento(0);
         }
     }
     public function salvarUsuario()
@@ -98,12 +117,13 @@ class Resultado extends Usuario
 
             if ($consulta->execute()) {
                 $this->setIdUsuario($pdo->lastInsertId());
-                $sqli = "INSERT INTO resultado VALUES (NULL,?,?,?,?)";
+                $sqli = "INSERT INTO resultado VALUES (NULL,?,?,?,?,?)";
                 $teste = $pdo->prepare($sqli);
                 $teste->bindValue(1, $this->getImc());
                 $teste->bindValue(2, $this->getData());
                 $teste->bindValue(3, $this->getClassificacao());
-                $teste->bindValue(4, $this->getIdUsuario());
+                $teste->bindValue(4, $this->getTipoTratamento());
+                $teste->bindValue(5, $this->getIdUsuario());
             }
             if ($teste->execute()) {
                 $_SESSION["idresultado"] = $pdo->lastInsertId();
@@ -155,17 +175,39 @@ class Resultado extends Usuario
                         </table>
                     ";
     }
-    public function salvarTratamento_resultado(){
+    public function salvarTratamento_resultado()
+    {
         include "config/conexao.php";
         $pdo->beginTransaction();
         $sql = "INSERT INTO resultado_tratamento VALUES (?,?)";
         $insert = $pdo->prepare($sql);
         $id1 = $_SESSION["idresultado"];
-        $id2 = $_SESSION["idtratamento"];
-        $insert->bindValue(1,$id1);
-        $insert->bindValue(2,$id2);
-        if($insert->execute()){
+        $insert->bindValue(1, $id1);
+        $insert->bindValue(2, $this->getIdTratamento());
+        if ($insert->execute()) {
             $pdo->commit();
+        }
+    }
+    public function select()
+    {
+        include "config/conexao.php";
+        $sql = " SELECT tratamento.id
+                 from tratamento 
+                 INNER JOIN resultado 
+                 on resultado.tipo_tratamento = tratamento.tipo_tratamento
+                 WHERE resultado.id = ?
+                 ORder BY RAND()
+                 LIMIT 1
+
+                ";
+        $select = $pdo->prepare($sql);
+        $select->bindValue(1, $this->getIdUsuario());
+
+        if ($select->execute()) {
+            $linha = $select->fetch(PDO::FETCH_OBJ);
+            $this->setIdTratamento($linha->id);
+        } else {
+            echo "erro ao salvar idtratamento";
         }
     }
 }
