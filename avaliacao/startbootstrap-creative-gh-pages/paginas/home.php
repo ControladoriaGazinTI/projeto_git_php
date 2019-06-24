c5e915eb1188c25bc7cb8f9e30aee98181bb0df0
JoasVieira <joasdouradina@gmail.com>
2019-06-24 19:30:25 -0300

    udate

--- avaliacao/startbootstrap-creative-gh-pages/paginas/home.php
+++ avaliacao/startbootstrap-creative-gh-pages/paginas/home.php
@@ -24,7 +24,7 @@
               <div class="row">
                   <div class="form-group col-6">
                       <label>Nome:</label>
-                      <input type="text" name="nome" class="form-control" required>
+                      <input type="text" name="nome" placeholder="Digite seu nome:" class="form-control" required>
                   </div>
                   <div class="form-group col-6">
                       <label>Data de nascimento:</label>
@@ -41,19 +41,19 @@
               </div>
               <div class="form-group">
                   <label>CPF:</label>
-                  <input type="text" name="cpf" class="form-control" required onkeypress="$(this).mask('000.000.000-00')">
+                  <input type="text" name="cpf" class="form-control" placeholder="Ex:000.000.000-00" required onkeypress="$(this).mask('000.000.000-00')">
               </div>
               <div class="row">
                   <div class="form-group col-6">
                       <label>Altura:</label>
-                      <input type="text" name="altura" class="form-control" required onkeypress="$(this).mask('0.00')">
+                      <input type="text" name="altura" class="form-control" placeholder="Ex:1.75" required onkeypress="$(this).mask('0.00')">
                   </div>
                   <div class="form-group col-6">
                       <label>Peso:</label>
-                      <input type="text" class="form-control" name="peso" onkeypress="$(this).mask('000.00')">
+                      <input type="text" class="form-control" name="peso" placeholder="Ex:72.5" onkeypress="$(this).mask('000.00')">
                   </div>
               </div>
-              <button type="submit" name="enviar" class="btn btn-primary ">Enviar</button>
+              <button type="submit" name="enviar" class="btn btn-primary " href="#navbarResponsive">Enviar</button>
           </form>
       </div>
       <div class="card-footer text-muted">
