a685527f721fa37e56bb898ee804bfd32eb31cf9
KiraGamesBR <romuloevil@outlook.com>
2019-07-30 15:01:48 -0300

    update

--- controle_de_producao/sistema/admin/cadastros/producao.php
+++ controle_de_producao/sistema/admin/cadastros/producao.php
@@ -4,44 +4,123 @@
             <h4 class="card-title">Cadastro de produção:</h4>
         </div>
         <div class="card-body pd-15">
-            <form method="POST" action="cad_cliente.php" enctype="multipart/form-data">
+            <form method="POST" action="salvar/producao" enctype="multipart/form-data">
                 <div class="form-group">
                     <label>Funcionario:</label>
-                    <select name="" id="" class="form-control" required="">
-                        <option value="">Selecione um Funcionario:</option>
-                        <option value="">joas</option>
-                        <option value="">romulo</option>
-                        <option value="">natiara</option>
+                    <select 
+                            name        ="idfuncionario" 
+                            class       ="form-control" 
+                            required    =""
+                    >
+                    <option value=""></option>
+                    <?php
+                            carregarOpcoes("id","funcionario","nome");
+                        ?>
                     </select>
                 </div>
                 <div class="form-group">
                     <label>Produto:</label>
-                    <select name="" id="" class="form-control" required="">
-                        <option value="">Selecione um Produto:</option>
-                        <option value="">cadeira</option>
-                        <option value="">mesa</option>
-                        <option value="">rack</option>
+                    <select 
+                        name        = "idproduto" 
+                        class       = "form-control" 
+                        required    = ""
+                    >
+                        <option value=""></option>
+                        <?php
+                            carregarOpcoes("id","produto","nome");
+                        ?>
                     </select>
                 </div>
-                <div class="row">
-                    <div class="form-group col-md-5">
-                        <label for="">Quatidade de Produçao:</label>
-                        <input type="number" class="form-control" required="required">
+                <div class="form-group">
+                        <label>Prioridade do pedido:</label>
+                        <select 
+                            name        = "prioridade" 
+                            class       = "form-control" 
+                            required    = ""
+                        >
+                            <option value= "">Selecione uma prioridade:</option>
+                            <option value= "Baixa">Baixa</option>
+                            <option value= "Média">Media</option>
+                            <option value= "Alta">Alta</option>
+                            ?>
+                        </select>
                     </div>
-                    <div class="form-grop col-md-5">
+                <div class="row">
+                    <div class="form-group col-md-6">
+                        <label>Quatidade de Produçao:</label>
+                        <input 
+                            name        ="qtde"
+                            type        ="number" 
+                            class       ="form-control" 
+                            required    ="required"
+                        >
+                    </div>
+                    <div class="form-grop col-md-6">
                         <label for="">Quantidade de Perdas:</label>
-                        <input type="number" class="form-control" required="required">
+                        <input
+                            name        ="qtde_perdas" 
+                            type        ="number" 
+                            class       ="form-control" 
+                            required    ="required"
+                        >
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-md-6">
+                        <label>Data entrega:</label>
+                        <input 
+                            name        ="data_ent"
+                            type        ="date"
+                            class       ="form-control"
+                            required
+                        >
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label>Data Lançamento:</label>
+                        <input
+                            name        ="data_lan" 
+                            type        ="date"
+                            class       ="form-control"
+                            required
+                        >
                     </div>
                 </div>
                 <div class="form-group">
-                        <label for="">Data de produção:</label>
-                        <input type="date" class="form-control" required="required">
+                <label for="descricao">Observação sobre a produção:</label>
+                    <textarea 
+                        id              ="descricao"
+                        name            ="observacao" 
+                        class           ="form-control"
+                        cols            ="1" 
+                        rows            ="1"
+                        maxlength       ="100"
+                    >
+                    </textarea>
                 </div>
             </div>
+
         <div class="card-footer pd-15">
-            <button type="submit" class="btn btn-fill btn-info">Enviar</button>
-            <button type="reset" class="btn btn-fill btn-danger">Cancelar</button>
+            <button type="submit" class="btn btn-fill btn-success">Cadastrar</button>
         </div>
         </form>
     </div>
 </div> 
+<script type="text/javascript">
+	$(document).ready(function() {
+		//aplicar o summernote
+		$("#descricao").summernote({
+			height: 200,
+            lang: 'pt-BR',
+            theme: 'monokai'
+		});
+    })
+    </script>
+     <script type="text/javascript">
+        $(document).ready(function() {
+            //aplica a mascara de valor no campo
+            $("#valor").maskMoney({
+                thousands: ".",
+                decimal: ","
+            });
+        })
+    </script>
---
+++ controle_de_producao/sistema/admin/salvar/producao.php
@@ -0,0 +1,62 @@
+<pre>
+<?php
+//verificar se a sessão esta ativa
+if (file_exists("verificalogin.php"))
+    include "verificalogin.php";
+else
+    include "../verificalogin.php";
+if ($_POST) {
+    print_r($_POST);
+    $id                     = "";
+    $idfuncionario          = "";
+    $idFuncionarioLogado    = $_SESSION["banco_tcc"]["id"];
+    $idproducao             = "";
+    $statusProducao         = false;
+    $statusItemProducao     = false;
+    $idproduto              = "";
+    $prioridade             = "";
+    $qtde                   = "";
+    $qtde_perdas            = "";
+    $data_ent               = "";
+    $data_lan               = "";
+    $observacao             = "";
+
+    foreach ($_POST as $key => $value) {
+        if (isset($_POST[$key])) {
+            $$key = trim($value);
+        }
+    }
+    print_r($idfuncionario);
+    if (empty($id)) {
+        $pdo->beginTransaction();
+        $sql = "INSERT INTO producao VALUES (NULL,?,?,?,?,?)";
+        $insert = $pdo->prepare($sql);
+        $insert->bindParam(1, $idFuncionarioLogado);
+        $insert->bindParam(2, $data_ent);
+        $insert->bindValue(3, $data_lan);
+        $insert->bindParam(4, $observacao);
+        $insert->bindParam(5, $statusProducao);
+        if ($insert->execute()) {
+            $idproducao = $pdo->lastInsertId();
+
+            $sql = "INSERT INTO item_producao VALUES (?,?,?,?,?,?,?)";
+            $insert = $pdo->prepare($sql);
+            $insert->bindParam(1, $idproduto);
+            $insert->bindParam(2, $idproducao);
+            $insert->bindParam(3, $idfuncionario);
+            $insert->bindParam(4, $prioridade);
+            $insert->bindParam(5, $qtde);
+            $insert->bindParam(6, $qtde_perdas);
+            $insert->bindParam(7, $statusItemProducao);
+
+          
+            if ($insert->execute()) {
+                $pdo->commit();
+                sucesso("Cadastratado com sucesso!!!","listar/producao");
+            } else {
+                $pdo->rollBack();
+               print_r($insert->errorInfo());
+            }
+        }
+    }
+}
